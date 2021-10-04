231cfb636b45526a107d68deb0f9aa9db6b54eed
Enes Malik Özer <enesmozer@gmail.com>
2021-10-04 23:49:53 +0300

    vuetify added

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="home" />
+  <div>
+    
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'Home',
+
   components: {},
 };
 </script>
