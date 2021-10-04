ede6857c831d6caacf68e9106fffc773efbb503b
Enes Malik Özer <enesmozer@gmail.com>
2021-10-04 22:23:06 +0300

    dotenv and db connection added to server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connection from './db.js';
+import todos from './routes/todo.js';
 
 const app = express();
-app.use(express.json());
+dotenv.config();
+
+app.use(json());
 app.use(cors());
+app.use('/api/v1/todos', todos);
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.listen(port, () =>
+  connection()
+    .then(console.log(`Listening on port ${port}`))
+    .catch((err) => console.log(err))
+);
