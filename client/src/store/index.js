ea3bf75f1b3e5e9f84e8a82f1af63f9b1445ceb1
Enes Malik Özer <enesmozer@gmail.com>
2021-10-06 16:33:22 +0300

    api call and implemted table

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import { cacheAdapterEnhancer } from 'axios-extensions';
+import * as constants from './constants';
 
 Vue.use(Vuex);
+const http = axios.create({
+  baseURL: 'http://localhost:5000/api/v1/',
+  adapter: cacheAdapterEnhancer(axios.defaults.adapter),
+});
 
 export default new Vuex.Store({
   state: {
+    allTodos: [],
   },
   mutations: {
+    [constants.SET_ALL_TODOS](state, allTodos) {
+      state.allTodos = allTodos;
+    },
   },
   actions: {
-  },
-  modules: {
+    async fetchTodos({ commit }) {
+      const res = await http.get('todos');
+      commit(constants.SET_ALL_TODOS, res.data);
+      return res.data;
+    },
   },
 });
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,25 +3,74 @@
     <div class="title">
       Welcome to Todo List
     </div>
+    <v-card class="list">
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        />
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="allTodos"
+        :search="search"
+      />
+    </v-card>
   </div>
 </template>
-
 <script>
+import { mapActions, mapState } from 'vuex';
+
 export default {
   name: 'Home',
 
   components: {},
+  data() {
+    return {
+      search: '',
+      headers: [
+        { text: 'Name', value: 'name' },
+        { text: 'Description', value: 'description' },
+      ],
+    };
+  },
+  computed: {
+    ...mapState(['allTodos']),
+  },
+  methods: {
+    ...mapActions(['fetchTodos']),
+    getTodos() {
+      this.fetchTodos();
+    },
+  },
+  created() {
+    this.getTodos();
+  },
 };
 </script>
 <style lang="scss" scoped>
 .home {
-    .title {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 4rem !important;
-        margin-block: 5rem;
-        height: 5rem;
-    }
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 4rem !important;
+    margin-block: 2.5rem;
+    height: 5rem;
+  }
+  .list {
+    width: 75%;
+  }
+  .text-start {
+    font-size: 1.5rem;
+  }
 }
 </style>
