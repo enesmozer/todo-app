e542dbe5a9233c8c5fc5579bda75669611879f23
Enes Malik Özer <enesmozer@gmail.com>
2021-10-07 23:06:57 +0300

    added e2e test to client side

--- client/tests/e2e/specs/test-with-pageobjects.js
+++ client/tests/e2e/specs/test-with-pageobjects.js
@@ -14,17 +14,10 @@ module.exports = {
     homepage.waitForElementVisible('@appContainer');
 
     const { app } = homepage.section;
-    app.assert.elementCount('@logo', 1);
-    app.expect.section('@welcome').to.be.visible;
-    app.expect.section('@headline').text.to.match(/^Welcome to Your Vue\.js (.*)App$/);
-
     browser.end();
   },
 
   'verify if string "e2e-nightwatch" is within the cli plugin links': (browser) => {
     const homepage = browser.page.homepage();
-    const welcomeSection = homepage.section.app.section.welcome;
-
-    welcomeSection.expect.element('@cliPluginLinks').text.to.contain('e2e-nightwatch');
   },
 };
